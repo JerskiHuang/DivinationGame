4a87a61ce3e2a44e7347b812151b646aa7fe9b14
JerskiHuang <jerski71@yahoo.com>
2021-12-30 18:07:58 +0800

    some modification

--- DivinationGame/ViewController.swift
+++ DivinationGame/ViewController.swift
@@ -6,14 +6,54 @@
 //
 
 import UIKit
+import SpriteKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var showDivinationImage: UIImageView!
+    
+    @IBOutlet weak var resultLabel: UILabel!
+    
+    var imageNumber = 0
+    let divinationImage = [1, 2, 3]
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        
+        let skView = SKView(frame: view.frame)
+        skView.backgroundColor = .clear
+        view.insertSubview(skView, at: 1)
+        let scene = SKScene(size: skView.frame.size)
+        scene.anchorPoint = CGPoint(x: 0.5, y: 0.5)
+        scene.backgroundColor = .clear
+        let emitterNode = SKEmitterNode(fileNamed: "MyParticle")
+        scene.addChild(emitterNode!)
+        skView.presentScene(scene)
+   
     }
+    func throwDivination () {
+            //random number
+            let index = Int.random(in: 0...12)
+            showDivinationImage.image = UIImage(named: "\(index)")
+            //based on the randon number connect to the different result
+            if index == 3 {
+                resultLabel.text = "Bad"
+            }else if index == 2 {
+                resultLabel.text = "Good"
+            }else if index == 1{
+                resultLabel.text = "Smile"
+            }
+        
+        }
 
-
+    @IBAction func executeThrow(_ sender: Any) {
+        
+        throwDivination()
+               
+    }
+    
 }
 
